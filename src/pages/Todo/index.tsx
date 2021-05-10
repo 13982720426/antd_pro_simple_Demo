97d200efe40245ffc430739eece990f6a666cd33
13982720426 <1679575151@qq.com>
2021-05-10 13:29:31 +0800

    Todolist初始化，固定列表数据

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,8 +1,14 @@
 import {PageContainer} from '@ant-design/pro-layout';
 import React from 'react';
+import {PlusOutlined} from '@ant-design/icons';
 import ProTable from '@ant-design/pro-table';
+import {Button, Alert} from 'antd';
 
-import {Button} from 'antd';
+const status = [
+  <Alert message="待办" type="info" showIcon />,
+  <Alert message="已完成" type="success" showIcon />,
+  <Alert message="已取消" type="error" showIcon />,
+];
 
 const columns = [
   {
@@ -16,12 +22,23 @@ const columns = [
   {
     title: '状态',
     dataIndex: 'status',
+    render: (_, record) => {
+      return status[record.status];
+    },
   },
   {
     title: '修改状态',
-    render: () => [<a>代办</a>, <a>完成</a>, <a>取消</a>],
+    render: () => [<a>待办 </a>, <a>完成 </a>, <a>取消</a>],
   },
 ];
+
+const data = [
+  {id: 1, title: 'TodoList列表', status: 0},
+  {id: 2, title: 'TodoList添加', status: 1},
+  {id: 3, title: 'TodoList编辑', status: 2},
+  {id: 4, title: 'TodoList修改状态', status: 0},
+];
+
 const Todo = () => {
   return (
     <PageContainer>
@@ -29,11 +46,12 @@ const Todo = () => {
         columns={columns}
         rowKey="id"
         search={false}
+        dataSource={data}
         dateFormatter="string"
         headerTitle="待办事项列表"
         toolBarRender={() => [
           <Button type="primary" key="primary">
-            新建
+            <PlusOutlined /> 新建
           </Button>,
         ]}
       />
